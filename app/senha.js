18351f815fd127d9e846d736cd910f71dae3e572
Igor da Silva Dias <artscolha@gmail.com>
2025-10-03 19:39:41 -0300

    validacao-de-senha-next-1 V3.0

--- app/senha.js
+++ app/senha.js
@@ -5,6 +5,9 @@ import { useState } from "react"
 export default function Senha() {
     const [senha, setSenha] = useState("")
     const [confirmeSenha, setConfirmeSenha] = useState("")
+
+    const [mostrarSenha, setMostrarSenha] = useState(false)
+    const [mostrarConfirmeSenha, setMostrarConfirmeSenha] = useState(false)
     function validar() {
         if (senha === "" || confirmeSenha === "") {
             alert("Por favor, preencha ambos os campos.")
@@ -21,19 +24,25 @@ export default function Senha() {
         setConfirmeSenha("")
     }
     return (
-        <div class="container">
+        <div className="container">
             <h1>Validador de senhas</h1>
-            <input type="password" placeholder="Digite a senha" id="senha" onChange={(e) => setSenha(e.target.value)} value={senha} onKeyDown={(e) => {
+            <input type={mostrarSenha ? "text" : "password"} placeholder="Digite a senha" id="senha" onChange={(e) => setSenha(e.target.value)} value={senha} onKeyDown={(e) => {
                 if (e.key === "Enter") {
                     validar()
                 }
             }} />
-            <input type="password" placeholder="Confirme a senha" id="confirmeSenha" onChange={(e) => setConfirmeSenha(e.target.value)} value={confirmeSenha} onKeyDown={(e) => {
+            <button type="button" onClick={() => setMostrarSenha(!mostrarSenha)} className="toggle-btn">
+                {mostrarSenha ? "😑" : "👀"}
+            </button>
+            <input type={mostrarConfirmeSenha ? "text" : "password"} placeholder="Confirme a senha" id="confirmeSenha" onChange={(e) => setConfirmeSenha(e.target.value)} value={confirmeSenha} onKeyDown={(e) => {
                 if (e.key === "Enter") {
                     validar()
                 }
             }} />
-            <button onClick={validar}>Validar</button>
+            <button type="button" onClick={() => setMostrarConfirmeSenha(!mostrarConfirmeSenha)} className="toggle-btn">
+                {mostrarConfirmeSenha ? "😑" : "👀"}
+            </button>
+            <button onClick={validar}>VALIDAR</button>
         </div>
     )
 }
